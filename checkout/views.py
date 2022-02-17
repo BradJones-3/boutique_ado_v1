56f203846e05d38db74ae284633d878f9de25f5d
BradJones-3 <bradjones_95@hotmail.com>
2022-02-17 17:41:25 +0000

    began adding Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KUDkJGqe9QmhagcpPTP3HDxRG8fBQLwMzPD7tKcGkPbnBG13zLRWWhvY6SdwNtAE3RWvR9RJswFmAwxBRHtmSet007JLiA6GO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

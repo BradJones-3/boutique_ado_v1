24cbcab6d23d0c936f78b350ff5c5c24cf42d32d
BradJones-3 <bradjones_95@hotmail.com>
2022-02-19 19:40:46 +0000

    added basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var stripe_client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe= Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var ClientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe= Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -34,4 +34,35 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        card.update({ 'disabled': true});
+        $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            // show error to your customer (e.g., insufficient funds)
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.onsubmit();
+            }
+        }
+    });
 });

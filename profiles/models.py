4409d6e3937fb86ef55c9fe5b90e436e97138434
BradJones-3 <bradjones_95@hotmail.com>
2022-02-26 01:17:47 +0000

    added order history and updated checkout views

--- profiles/models.py
+++ profiles/models.py
@@ -8,7 +8,7 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    A user prpfile model for maintaining default
+    A user profile model for maintaining default
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -18,8 +18,8 @@ class UserProfile(models.Model):
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label="Country", null=True, blank=True)
-    
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
+
     def __str__(self):
         return self.user.username
 
@@ -31,5 +31,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Existing user: just use the profile
+    # Existing users: just save the profile
     instance.userprofile.save()
